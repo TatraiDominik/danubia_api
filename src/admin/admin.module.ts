d9187eba91369210275f7ef9bb701564eac979cb
TatraiDominik <tatraidominikoszkar@turr.hu>
2025-03-30 20:05:43 +0200

    feat: added admin user

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminService } from './admin.service';
 import { Admin, AdminSchema } from 'src/types/schema/Admin';
+import { AdminController } from './admin.controller';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }])],
   providers: [AdminService],
+  controllers: [AdminController],
   exports: [AdminService],  
 })
 export class AdminModule {}
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Admin, AdminDocument } from 'src/types/schema/Admin';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AdminService {
@@ -13,5 +14,14 @@ export class AdminService {
 
   async findById(id: string): Promise<AdminDocument | null> {
     return this.adminModel.findById(id).exec();
+  }
+
+
+  async validateAdminCredentials(adminName: string, password: string): Promise<AdminDocument | null> {
+    const admin = await this.adminModel.findOne({ adminName }).exec();
+    if (admin && await argon2.verify(admin.password, password)) {
+      return admin;
+    }
+    return null;
   }
 }
