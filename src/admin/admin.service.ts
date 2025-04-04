61a279cba612df2dbf965e4a614af6cf9d934cf3
TatraiDominik <tatraidominikoszkar@turr.hu>
2025-04-05 00:32:15 +0200

    fix: took out admin routes, cors settings

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Admin, AdminDocument } from 'src/types/schema/Admin';
+import { Admin, AdminDocument } from 'src/types/schema/Admin'; 
 import * as argon2 from 'argon2';
 
 @Injectable()
@@ -16,7 +16,6 @@ export class AdminService {
     return this.adminModel.findById(id).exec();
   }
 
-
   async validateAdminCredentials(adminName: string, password: string): Promise<AdminDocument | null> {
     const admin = await this.adminModel.findOne({ adminName }).exec();
     if (admin && await argon2.verify(admin.password, password)) {
@@ -24,4 +23,7 @@ export class AdminService {
     }
     return null;
   }
+
+
 }
+
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.setGlobalPrefix('danubia_api');
-  await app.listen(process.env.PORT ?? 3000);
 
+  // CORS beállítások
+  app.enableCors({
+    origin: '*', // Ezt később szigorítani kell, pl.: ['http://localhost:5173']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  app.setGlobalPrefix('danubia_api');
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
+
